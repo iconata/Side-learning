d6dd322d43b9a75620dbfd9084de47cf247c38b9
Hristo Vasilev <hristo39@gmail.com>
2022-04-13 15:44:51 +0300

    updated some more files

--- C/test.c
+++ C/test.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 
 int main(void)
-{	int i;
-	char c[30];
+{
+	int i = 55;
+	
+	char c = "hello dude";
 
-	scanf("%s", c);
-	if (sizeof(c) <= 3)
-	{
-		printf("%s", c);
-	}
-	else
-	{
-		for (i; i < 30; i++)
-	{
-		printf("%s", c[i]);
-	}}
-	printf("Hello, %s", c);
+	int *pntr = &i;
+
+	char *pointer = &c;
+
+	printf("%p\n", pntr);
+
+	printf("%p\n", pointer);
 }
