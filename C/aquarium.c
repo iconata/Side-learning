d6b1e26d4d27375da980295ab510af4dc2cbeb0c
Hristo Vasilev <hristo39@gmail.com>
2022-04-14 13:42:34 +0300

    another

--- C/aquarium.c
+++ C/aquarium.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-int main(){
-	int lenght;
-	int width;
-	int height;
-	int percent;
+void main(){
+	float lenght;
+	float width;
+	float height;
+	float percent;
 
-	scanf("%d", &lenght);
-	scanf("%d", &width);
-	scanf("%d", &height);
-	scanf("%d", &percent);
+	scanf("%f", &lenght);
+	scanf("%f", &width);
+	scanf("%f", &height);
+	scanf("%f", &percent);
+
+	float volume = lenght * width * height;
+	float volume_in_liters = volume / 1000;
+	float volume_remaining = 100 - percent;
+	float convert = volume_remaining / 100;
+	float liters_remaining = volume_in_liters * convert;
+	printf("%f", liters_remaining);
 }
