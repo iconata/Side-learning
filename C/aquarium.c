c26de23a5a9c2ef5b7eca7fd44564f1799f25d4d
Hristo Vasilev <hristo39@gmail.com>
2022-04-14 13:22:25 +0300

    updated .gitignore

---
+++ C/aquarium.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int lenght;
+	int width;
+	int height;
+	int percent;
+
+	scanf("%d", &lenght);
+	scanf("%d", &width);
+	scanf("%d", &height);
+	scanf("%d", &percent);
+}
