577d8bbe319d04ec8558333ab35d72c8040e80be
Hristo Vasilev <hristo39@gmail.com>
2022-04-22 12:10:05 +0300

    added one more

---
+++ Hackerrank challenges/C/max_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d);
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
+int max_of_four(int a, int b, int c, int d){
+    if (a > b) {
+        if (a > c) {
+            if (a > d) {
+                return a;
+            }else {
+                return d;
+            }
+        }
+    }if (b > c) {
+        if (b > d){
+            return b;
+        }else {
+            return d;
+        }
+    }if (c > d) {
+        return c;
+    }else {
+        return d;
+    };
+    return a | b | c | d;
+}
